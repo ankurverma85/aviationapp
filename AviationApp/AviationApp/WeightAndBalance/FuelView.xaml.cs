dc1a8abd67f64cd0742c894bb78512e92bb87187
Skand Hurkat <skandhurkat@gmail.com>
2020-03-28 16:49:38 -0700

    Add a very bad example of a fuel station widget

---
+++ AviationApp/AviationApp/WeightAndBalance/FuelView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AviationApp.WeightAndBalance
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FuelView : ContentView
+    {
+        public FuelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AviationApp/AviationApp/WeightAndBalance/FuelViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace AviationApp.WeightAndBalance
+{
+    class FuelViewModel : INotifyPropertyChanged
+    {
+        public FuelViewModel()
+        { arm = 0.0f; capacity = 0.0f; fuelWeightInKg = 0.0f; }
+        public FuelViewModel(float arm, float capacity)
+        {
+            this.arm = arm;
+            this.capacity = capacity;
+            fuelWeightInKg = 0.0f;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private float _fuelWeightInKg;
+        public float fuelWeightInKg {
+            get => _fuelWeightInKg;
+            set
+            {
+                _fuelWeightInKg = value;
+                var args = new PropertyChangedEventArgs(nameof(FuelVolumeGal));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+        public float arm { get; }
+        public float capacity { get; }
+        public float FuelWeightInLb
+        {
+            get => (fuelWeightInKg * 2.204623f);
+        }
+        public float FuelVolumeGal
+        {
+            get => (fuelWeightInKg / 6.0f);
+        }
+    }
+}
