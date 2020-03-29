ac049ea8f0dbd9573508c6095c3ad8f0304d3cd6
Skand Hurkat <skandhurkat@gmail.com>
2020-03-28 18:23:22 -0700

    Change fuel quantity display with units

--- AviationApp/AviationApp/WeightAndBalance/FuelStationView.xaml.cs
+++ AviationApp/AviationApp/WeightAndBalance/FuelStationView.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AviationApp.WeightAndBalance
 {
+    enum FuelQuantityUnits { kg, lb, l, gal };
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FuelStationView : ContentView
     {
@@ -12,17 +14,43 @@ namespace AviationApp.WeightAndBalance
         {
             InitializeComponent();
         }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            foreach (FuelQuantityUnits u in Enum.GetValues(typeof(FuelQuantityUnits)))
+            {
+                unitsPicker.Items.Add(u.ToString());
+            }
+        }
     }
 
     class FuelStationViewModel : INotifyPropertyChanged
     {
         public FuelStationViewModel()
         { arm = 0.0f; capacity = 0.0f; fuelWeightInKg = 0.0f; }
-        public FuelStationViewModel(float arm, float capacity)
+        public FuelStationViewModel(float arm, float capacity, int displayUnits)
         {
             this.arm = arm;
             this.capacity = capacity;
             fuelWeightInKg = 0.0f;
+            this.displayUnits = displayUnits;
+        }
+        private FuelQuantityUnits _displayUnits;
+        public int displayUnits
+        {
+            get
+            {
+                return (int)_displayUnits;
+            }
+            set
+            {
+                _displayUnits = (FuelQuantityUnits)value;
+                var unitChangeArgs = new PropertyChangedEventArgs(nameof(displayUnits));
+                PropertyChanged?.Invoke(this, unitChangeArgs);
+                var displayQuantityChangeArgs = new PropertyChangedEventArgs(nameof(displayFuel));
+                PropertyChanged?.Invoke(this, displayQuantityChangeArgs);
+            }
         }
         public event PropertyChangedEventHandler PropertyChanged;
         private float _fuelWeightInKg;
@@ -32,19 +60,26 @@ namespace AviationApp.WeightAndBalance
             set
             {
                 _fuelWeightInKg = value;
-                var args = new PropertyChangedEventArgs(nameof(FuelVolumeGal));
+                var args = new PropertyChangedEventArgs(nameof(displayFuel));
                 PropertyChanged?.Invoke(this, args);
             }
         }
         public float arm { get; }
         public float capacity { get; }
-        public float FuelWeightInLb
+        public float displayFuel
         {
-            get => (fuelWeightInKg * 2.204623f);
-        }
-        public float FuelVolumeGal
-        {
-            get => (fuelWeightInKg / 6.0f);
+            get
+            {
+                switch ((FuelQuantityUnits)displayUnits)
+                {
+                    case FuelQuantityUnits.kg: return fuelWeightInKg;
+                    case FuelQuantityUnits.lb: return fuelWeightInKg * 2.204623f;
+                    case FuelQuantityUnits.gal: return fuelWeightInKg * 2.204623f / 6.01f;
+                    case FuelQuantityUnits.l: return fuelWeightInKg * 2.204623f / 6.01f * 3.7854f;
+                }
+                // Code should never reach here
+                return float.NaN;
+            }
         }
     }
 }
