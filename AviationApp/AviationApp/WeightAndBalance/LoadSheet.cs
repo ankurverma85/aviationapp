040460515aafdc3790e728a588b19c71490e2684
Skand Hurkat <skandhurkat@gmail.com>
2020-03-29 17:48:43 -0700

    Stash partially completed LoadSheet class

---
+++ AviationApp/AviationApp/WeightAndBalance/LoadSheet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AviationApp.Utilities.Units;
+using AviationApp.Utilities.Quantities;
+
+namespace AviationApp.WeightAndBalance
+{
+
+    class LoadSheet
+    {
+        public string AircraftIdentifier { get; set; } = "";
+        public string AircraftType { get; set; } = "Unknown";
+        public List<LoadStation> LoadStations { get; set; } = new List<LoadStation> { };
+        public List<FuelStation> FuelStations { get; set; } = new List<FuelStation> { };
+    }
+    class LoadStation
+    {
+        public string StationName { get; set; } = "";
+        public Length StationArm { get; set; } = new Length();
+        public List<StationItem> StationItems { get; set; } = new List<StationItem> { };
+        public Mass MaximumMass { get; set; } = new Mass { KiloGrams = Double.PositiveInfinity };
+        public Mass TotalMass { get { Mass m = new Mass(); foreach (var si in StationItems) { m += si.Mass; } return m; } }
+        public bool MaximumMassExceeded { get => TotalMass > MaximumMass; }
+    }
+    class StationItem
+    {
+        public string ItemName { get; set; } = "";
+        public Mass Mass { get; set; } = new Mass();
+    }
+    class FuelStation
+    {
+        public string Name { get; set; } = "";
+        public Length Arm { get; } = new Length();
+        public IFuel Fuel { get; set; }
+        public IFuel Capacity { get; }
+        public Mass Mass { get => Fuel.Mass; }
+    }
+}
