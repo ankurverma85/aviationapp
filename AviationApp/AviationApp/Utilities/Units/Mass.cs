e68fbc9be833d0676cc416beb714be0a2a707678
Skand Hurkat <skandhurkat@gmail.com>
2020-03-29 17:24:43 -0700

    Add comparison and addition to Mass

--- AviationApp/AviationApp/Utilities/Units/Mass.cs
+++ AviationApp/AviationApp/Utilities/Units/Mass.cs
@@ -5,7 +5,10 @@
         private const double LB_IN_KG = 2.204623;
         private double weightInKg = 0.0;
         public double KiloGrams { get => weightInKg; set { weightInKg = value; } }
-        public double Pounds { get => weightInKg * LB_IN_KG;  set { weightInKg = value / LB_IN_KG; } }
+        public double Pounds { get => weightInKg * LB_IN_KG; set { weightInKg = value / LB_IN_KG; } }
         public double Grams { get => weightInKg * 1000; set { weightInKg = value / 1000; } }
+        public static Mass operator +(Mass a, Mass b) => new Mass { weightInKg = a.weightInKg + b.weightInKg };
+        public static bool operator <(Mass a, Mass b) => a.weightInKg < b.weightInKg;
+        public static bool operator >(Mass a, Mass b) => a.weightInKg > b.weightInKg;
     }
 }
