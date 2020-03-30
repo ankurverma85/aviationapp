c4de051fbf1656a7b1f97c393564931551bf7d08
Skand Hurkat <skandhurkat@gmail.com>
2020-03-29 17:41:29 -0700

    Update Volume.cs, use auto property

--- AviationApp/AviationApp/Utilities/Units/Volume.cs
+++ AviationApp/AviationApp/Utilities/Units/Volume.cs
@@ -4,10 +4,10 @@
     {
         private const double USGAL_IN_MCUBE = 264.17205124156;
         private const double IMPGAL_IN_MCUBE = 219.96924829909;
-        private double volumeInMCubed = 0.0;
-        public double CubicMetre { get => volumeInMCubed; set { volumeInMCubed = value; } }
-        public double Litre { get => volumeInMCubed * 1000.0; set { volumeInMCubed = value / 1000.0; } }
-        public double USGallon { get => volumeInMCubed * USGAL_IN_MCUBE; set { volumeInMCubed = value / USGAL_IN_MCUBE; } }
-        public double ImperialGallon { get => volumeInMCubed * IMPGAL_IN_MCUBE; set { volumeInMCubed = value / IMPGAL_IN_MCUBE; } }
+
+        public double CubicMetre { get; set; } = 0.0;
+        public double Litre { get => CubicMetre * 1000.0; set { CubicMetre = value / 1000.0; } }
+        public double USGallon { get => CubicMetre * USGAL_IN_MCUBE; set { CubicMetre = value / USGAL_IN_MCUBE; } }
+        public double ImperialGallon { get => CubicMetre * IMPGAL_IN_MCUBE; set { CubicMetre = value / IMPGAL_IN_MCUBE; } }
     }
 }
