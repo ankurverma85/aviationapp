1f570062c839f11858a9b981ab4850ce3fabcfb0
Skand Hurkat <skandhurkat@gmail.com>
2020-04-04 17:39:13 -0700

    Create placeholder pages and tabbed shell navigation

---
+++ AviationApp/AviationApp/Pages/MapPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AviationApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
