563f0a941b6e57d385d3336e20d9a48cbbd9d47f
Skand Hurkat <skandhurkat@users.noreply.github.com>
2020-04-07 00:16:29 -0400

    Icons for shell navigation (#7)
     - Use material font icons for shell navigation targets
        - Provide helper class for getting glyph unicode
        - Does not work on UWP for some reason
    
     - Updated Xamarin.Forms and Xamarin.Essentials version
        - Xamarin.Forms must be updated to support embedding fonts in assembly

---
+++ AviationApp/AviationApp/Assets/Fonts/MaterialFontGlyphs.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialFonts")]
+
+namespace AviationApp.Assets.Fonts
+{
+    public static class MaterialFontGlyphs
+    {
+        public static string Map = "\U000f034d";
+        public static string Planning = "\U000f05d5";
+        public static string WeightAndBalance = "\U000f05d1";
+        public static string Briefing = "\U000f0890";
+        public static string FlightComputer = "\U000f00ec";
+        public static string Checklists = "\U000f0756";
+        public static string Scratchpad = "\U000f039a";
+        public static string AirportInformation = "\U000f064e";
+        public static string Logbook = "\U000f05da";
+        public static string Settings = "\U000f0493";
+    }
+}
