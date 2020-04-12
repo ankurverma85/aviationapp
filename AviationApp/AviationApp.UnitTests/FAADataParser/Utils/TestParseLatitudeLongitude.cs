bbb702c9bdf83d5074839d8d997b2ddc09dd7f7e
Skand Hurkat <skandhurkat@gmail.com>
2020-04-12 11:50:18 -0700

    Parse latitude and longitude

---
+++ AviationApp/AviationApp.UnitTests/FAADataParser/Utils/TestParseLatitudeLongitude.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using AviationApp.FAADataParser.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AviationApp.UnitTests.FAADataParser.Utils
+{
+    [TestClass]
+    public class TestParseLatitudeLongitude
+    {
+        [TestMethod]
+        public void GoodLatitudeParsingTests()
+        {
+            List<(string, double)> dataList = new List<(string, double)>
+            {
+                (@"34-36-21.290N ", 34.60591389),
+                (@"47-38-18.000N ", 47.63833333),
+                (@"25-57-38.954S ", -25.96082056)
+            };
+            foreach ((string, double) elem in dataList)
+            {
+                double result = ParseLatitudeLongitude.TryParse(elem.Item1);
+                Assert.AreEqual(elem.Item2, result, 0.00000001);
+            }
+        }
+        [TestMethod]
+        public void GoodLongitudeParsingTests()
+        {
+            List<(string, double)> dataList = new List<(string, double)>
+            {
+                (@"134-36-21.290E", 134.60591389),
+                (@"047-38-18.000E", 47.63833333),
+                (@"125-57-38.954W", -125.96082056)
+            };
+            foreach ((string, double) elem in dataList)
+            {
+                double result = ParseLatitudeLongitude.TryParse(elem.Item1);
+                Assert.AreEqual(elem.Item2, result, 0.00000001);
+            }
+        }
+    }
+}
---
+++ AviationApp/AviationApp/FAADataParser/Utils/ParseLatitudeLongitude.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AviationApp.FAADataParser.Utils
+{
+    public static class ParseLatitudeLongitude
+    {
+        public static double TryParse(string latitudeString)
+        {
+            Match match = latlonRegex.Match(latitudeString);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Bad latitude");
+            }
+            if (!int.TryParse(match.Groups["Degrees"].Value, out int degrees))
+            {
+                throw new ApplicationException("Bad latitude");
+            }
+            if (!int.TryParse(match.Groups["Minutes"].Value, out int minutes))
+            {
+                throw new ApplicationException("Bad latitude");
+            }
+            if (!double.TryParse(match.Groups["Seconds"].Value, out double seconds))
+            {
+                throw new ApplicationException("Bad latitude");
+            }
+            bool negative;
+            switch (match.Groups["Hemisphere"].Value)
+            {
+                case "N": negative = false; break;
+                case "S": negative = true; break;
+                case "E": negative = false; break;
+                case "W": negative = true; break;
+                default: throw new ApplicationException("Invalid hemisphere");
+            }
+            double latitude = (negative ? -1.0 : 1.0) * (degrees + (minutes/60.0) + (seconds/3600.0));
+            return latitude;
+        }
+        private static readonly Regex latlonRegex = new Regex(@"(?<Degrees>\d*)-(?<Minutes>\d*)-(?<Seconds>\d*\.\d*)(?<Hemisphere>[NSEW])\s*");
+    }
+}
