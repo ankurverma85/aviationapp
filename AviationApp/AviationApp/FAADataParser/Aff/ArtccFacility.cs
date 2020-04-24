87c8b0668d3445a1c15a4428813af6186509a8b0
Skand Hurkat <skandhurkat@gmail.com>
2020-04-23 19:22:42 -0700

    Add basic Aff file parser and associated output classes

---
+++ AviationApp/AviationApp/FAADataParser/Aff/ArtccFacility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviationApp.FAADataParser.Aff
+{
+    public class ArtccFacility
+    {
+        public string ArtccIdent { get; set; }
+        public string ArtccName { get; set; }
+        public string SiteLocation { get; set; }
+        public string AltName { get; set; }
+        public FacilityType FacilityType { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string IcaoId { get; set; }
+        public List<(int number, string remark)> FacilityRemarks { get; set; } = new List<(int number, string remark)>();
+        public List<ArtccFrequencyInformation> Frequencies { get; set; } = new List<ArtccFrequencyInformation>();
+    }
+}
---
+++ AviationApp/AviationApp/FAADataParser/Aff/ArtccFrequencyInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviationApp.FAADataParser.Aff
+{
+    public class ArtccFrequencyInformation
+    {
+        public decimal Frequency { get; set; }
+        public AltitudeSector AltitudeSector { get; set; }
+        public FrequencySpecialUsage FrequencySpecialUsage { get; set; }
+        public bool RCAGFrequencyCharted { get; set; }
+        public bool AirportInformationAvailable { get; set; }
+        public string AirportIdent { get; set; }
+        public string AirportState { get; set; }
+        public string AirportStatePOCode { get; set; }
+        public string AirportCity { get; set; }
+        public string AirportName { get; set; }
+        public double AirportLatitude { get; set; }
+        public double AirportLongitude { get; set; }
+        public List<(int number, string remark)> Remarks { get; set; } = new List<(int number, string remark)>();
+    }
+}
