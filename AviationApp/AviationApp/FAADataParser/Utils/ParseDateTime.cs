b9c03f6fa826718338fb850dfd77da054c022aca
Skand Hurkat <skandhurkat@gmail.com>
2020-05-03 16:31:07 -0700

    Add parser for MM/dd/yyyy dates

---
+++ AviationApp/AviationApp/FAADataParser/Utils/ParseDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviationApp.FAADataParser.Utils
+{
+    class ParseDateTime
+    {
+        public static bool TryParse(string val, out DateTime dateTime)
+        {
+            return DateTime.TryParseExact(val, "MM/dd/yyyy", null, System.Globalization.DateTimeStyles.None, out dateTime);
+        }
+    }
+}
