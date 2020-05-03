dfbe27a3fda7b75475ce800f05410ad31308831f
Skand Hurkat <skandhurkat@gmail.com>
2020-05-03 11:23:41 -0700

    Add generic FAADataParser

---
+++ AviationApp/AviationApp.UnitTests/FAADataParser/Utils/TestFAADataParserGeneric.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AviationApp.FAADataParser.Fixes;
+using AviationApp.FAADataParser.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AviationApp.UnitTests.FAADataParser.Utils
+{
+    [TestClass]
+    public class TestFAADataParserGeneric
+    {
+        [TestMethod]
+        public void TestParseFix1()
+        {
+            List<(int fieldBegin, int fieldLength, Type parserType, string propertyName, bool nullable)> parseFields = new List<(int fieldBegin, int fieldLength, Type parserType, string propertyName, bool nullable)>
+            {
+                (4, 30, typeof(string), nameof(Fix1.FixID), false),
+                (34, 30, typeof(string), nameof(Fix1.State), false),
+                (64, 2, typeof(string), nameof(Fix1.ICAORegionCode), false),
+                (66, 14, typeof(ParseLatitudeLongitude), nameof(Fix1.Latitude), false),
+                (80, 14, typeof(ParseLatitudeLongitude), nameof(Fix1.Longitude), false)
+            };
+            string fix1String = @"FIX1AARTA                         ALABAMA                       K734-36-21.290N 087-16-24.750WFIX                                                                                                                   YWAYPOINT       AARTAZME ZME                               NNN                                                                                                                                                                                                ";
+
+            Assert.IsTrue(FAADataParserGeneric<Fix1>.TryParse(fix1String, 466, parseFields, out Fix1 fix));
+            // Try parsing a real string from the database
+            Assert.AreEqual("AARTA", fix.FixID);
+            Assert.AreEqual("ALABAMA", fix.State);
+            Assert.AreEqual("K7", fix.ICAORegionCode);
+            Assert.AreEqual(124581.290m, fix.Latitude);
+            Assert.AreEqual(-314184.750m, fix.Longitude);
+            // Assert.AreEqual(FixType.Civilian, fix.FixType);
+            // Assert.AreEqual(FixUse.Waypoint, fix.FixUse);
+            // Assert.AreEqual("AARTA", fix.NASIdentifier);
+            // Assert.AreEqual("ZME", fix.LowARTCC);
+            // Assert.AreEqual("ZME", fix.HighARTCC);
+        }
+    }
+}
---
+++ AviationApp/AviationApp/FAADataParser/Utils/FAADataParserGeneric.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AviationApp.FAADataParser.Utils
+{
+    class FAADataParserGeneric<T> where T : new()
+    {
+        public static bool TryParse(
+            string input,
+            int expectedInputLength,
+            List<(int fieldBegin, int fieldLength, Type parserType, string propertyName, bool nullable)> fieldList,
+            out T output
+            )
+        {
+            output = new T();
+            Type t = output.GetType();
+            if (input.Length != expectedInputLength)
+            {
+                return false;
+            }
+            foreach (var field in fieldList)
+            {
+                PropertyInfo property = t.GetProperty(field.propertyName);
+                string fieldSubstring = input.Substring(field.fieldBegin, field.fieldLength).Trim();
+                if (fieldSubstring.Length == 0)
+                {
+                    if (field.nullable)
+                    {
+                        property.SetValue(output, null);
+                    }
+                    else
+                    { return false; }
+                }
+                else if (field.parserType == typeof(string))
+                {
+                    property.SetValue(output, fieldSubstring);
+                }
+                else
+                {
+                    MethodInfo method = field.parserType.GetMethod("TryParse");
+                    object[] parameters = new object[] { fieldSubstring, null };
+                    object result = method.Invoke(null, parameters);
+                    bool boolResult = (bool)result;
+                    if (boolResult)
+                    {
+                        property.SetValue(output, parameters[1]);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
